a8067ea046fea20827ac0f283c6d074c05fbab77
IzzzGoy <43094593+IzzzGoy@users.noreply.github.com>
2021-09-15 18:36:40 +0300

    Init

---
+++ src/main/kotlin/Calculator.kt
@@ -0,0 +1,11 @@
+/**
+ * Create a simple calculator, that consume expressions like 1 + 2 or 5 * 1.
+ * Expression must contain two numbers and an operation.
+ * Calculator must handle exceptions like: Divide by zero,  more, then one operation and wrong quantity of numbers in expression.
+ * @param expression that calculator must solve
+ * @return result of calculation
+ * */
+
+fun calculate(expression: String): Double {
+    return 0.0
+}
