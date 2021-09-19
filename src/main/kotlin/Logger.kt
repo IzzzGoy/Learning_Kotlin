2fc2b30528d658c11730898e4f8e4553021f1ac1
IzzzGoy <43094593+IzzzGoy@users.noreply.github.com>
2021-09-19 17:35:51 +0300

    Logger task added

---
+++ src/main/kotlin/Logger.kt
@@ -0,0 +1,8 @@
+/**
+ * Create two Loggers, that represent two ways of logging: logging into console and logging into file.
+ * There are no tests. So, made yours own tests for each logger.
+ * */
+
+interface Logger {
+    fun makeLog(message: String)
+}
